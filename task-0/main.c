87b1fc005264ac008b99b4b3d3cbf8c1236b336f
mike <surinmike@gmail.com>
2014-02-28 17:36:30 +0400

    task-0 intial commit

---
+++ task-0/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#define MAXN 100000
+
+int * arr;
+int ** order;
+int n;
+
+void readarr(int n) {
+}
+
+void randomarr(int n) {
+    
+}
+
+int main(int argc, char ** argv) {
+    int i;
+    for (i = 0; i < argc; i++) {
+        if (argc != 0 && strcmp("-n", argv[argc - 1]) == 0) {
+            
+        }
+        if (strcmp("-r", argv[argc - 1]) == 0) {
+            
+        }
+    }
+}
