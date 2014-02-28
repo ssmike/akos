2517dc6678f12a5cc57d8666eebf91b9ab821f01
mike <surinmike@gmail.com>
2014-02-28 15:01:26 +0400

    ansi compliance added

--- task-1/getss.h
+++ task-1/getss.h
@@ -2,9 +2,10 @@
 #define GETSS_H
 #define GETSS_DEFAULT_LIM 10000
 #include <stdio.h>
-
-//TODO: add windows support
-//all functions return negative values on errors
+/*
+ * TODO: add windows support
+ * all functions return negative values on errors
+ */
 int getss(char ** res);
 int getss_(char ** res, int lim);
 int fgetss(FILE * fin, char ** res);
