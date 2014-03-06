e74a9655d3a4911a472baa6793130f9c21d93508
mike <surinmike@gmail.com>
2014-03-06 22:27:28 +0400

    fixed unsafe malloc && realloc

--- task-1/getss.c
+++ task-1/getss.c
@@ -23,24 +23,32 @@ int fgetss_(FILE * fin, char ** res, int lim) {
     if (feof(stdin)) {
         return EOF_ERROR;
     }
+    *res = NULL;
     int sz = 0;
     int allsz = 1;
-    char * buf = malloc(sizeof(char));
+    char * buf = malloc(sizeof(char)), * tmp;
+    if (buf == NULL) return ALLOCATION_FAILED;
     int cchar;
-    while (sz < lim && (cchar = fgetc(fin)) != '\n') {
-       if (sz + 1 > allsz) {
-           buf = realloc(buf, allsz * 2 * sizeof(char));
-           allsz *= 2;
-       }
-       if (buf == NULL)  return ALLOCATION_FAILED;
-       buf[sz++] = cchar;
+    while (sz < lim && (cchar = fgetc(fin)) != '\n'&& !feof(fin)) {
+        if (sz + 1 > allsz) {
+            tmp = realloc(buf, allsz * 2 * sizeof(char));
+            if (tmp == NULL) {
+                *res = buf;
+                return ALLOCATION_FAILED;
+            }
+            allsz *= 2;
+        } 
+        buf[sz++] = cchar;
     }
     if (sz >= lim) {
         *res = buf;
         return LIMIT_EXCEEDED;
     }
     buf = realloc(buf, (sz + 1) * sizeof(char));
-    if (buf == NULL)  return ALLOCATION_FAILED;
+    if (tmp == NULL) {
+        *res = buf;
+        return ALLOCATION_FAILED;
+    }
     buf[sz] = '\0';
     *res = buf;
     return sz;
