9c26dc28b14e21e6f05ba1699de856d05e943939
mike <surinmike@gmail.com>
2014-03-07 18:23:49 +0400

    fixed exponential memory allocation

--- task-1/getss.c
+++ task-1/getss.c
@@ -1,17 +1,12 @@
 #include "getss.h"
 #include <stdlib.h>
-
-/*TODO
- * 
- * Можно было бы и макросами сделать
- * наверно.  
- */
+#include <unistd.h>
 
 int getss(char ** res) {
     return fgetss(stdin, res);
 }
 
-int getss_(char ** res, int lim) {
+int getss_(char ** res, size_t lim) {
     return fgetss_(stdin, res, lim);
 }
 
@@ -19,50 +14,47 @@ int fgetss(FILE * fin, char ** res) {
     return fgetss_(fin, res, GETSS_DEFAULT_LIM);
 }
 
-#ifdef __unix__
-#define line_end '\n'
-#else
-#define line_end '\r'
-#endif
-
-int fgetss_(FILE * fin, char ** res, int lim) {
+int fgetss_(FILE * fin, char ** res, size_t lim) {
     *res = NULL;
-    int cchar;
-    int sz = 0;
-    int allsz = 1;
+    int cchar, psize = getpagesize();
+    size_t sz = 0;
+    int allsz = sizeof(char);
     char * buf, * tmp;
     if (feof(stdin)) {
         return EOF_ERROR;
     }
     buf = malloc(sizeof(char));
     if (buf == NULL) return ALLOCATION_FAILED;
-    while (sz < lim && (cchar = fgetc(fin)) != line_end && !feof(fin)) {
-        while (sz + 1 >= allsz) {
-            tmp = realloc(buf, allsz * 2 * sizeof(char));
+    while (sz < lim && (cchar = fgetc(fin)) != '\n' && !feof(fin)) {
+        while (sz + sizeof(char) >= allsz) {
+            size_t newbsz = allsz * 2;
+            if (allsz > PAGES_LIMIT * psize) {
+                if (allsz % psize != 0) {
+                    newbsz = allsz + psize - (allsz % psize);
+                } else {
+                    newbsz = allsz + psize;
+                }
+            }
+            tmp = realloc(buf, newbsz);
             if (tmp == NULL) {
                 *res = buf;
                 return ALLOCATION_FAILED;
             } else buf = tmp;
-            allsz *= 2;
+            allsz = newbsz;
         } 
-        buf[sz++] = cchar;
+        buf[sz/sizeof(char)] = cchar;
+        sz += sizeof(char);
     }
     if (sz >= lim) {
         *res = buf;
         return LIMIT_EXCEEDED;
     }
-    tmp = realloc(buf, (sz + 1) * sizeof(char));
+    tmp = realloc(buf, sz + sizeof(char));
     if (tmp == NULL) {
         *res = buf;
         return ALLOCATION_FAILED;
     } else buf = tmp;
-    buf[sz] = '\0';
+    buf[sz/sizeof(char)] = '\0';
     *res = buf;
-#ifndef __unix__
-    {
-        char tmp_char; 
-        while((tmp_char = fgetc(fin)) != '\n' && !feof(fin));
-    }
-#endif
     return sz;
 }
--- task-1/getss.h
+++ task-1/getss.h
@@ -7,10 +7,11 @@
 #define ALLOCATION_FAILED (-1)
 #define EOF_ERROR (-2)
 #define LIMIT_EXCEEDED (-3)
+#define PAGES_LIMIT 10
 
 extern int getss(char ** res);
-extern int getss_(char ** res, int lim);
+extern int getss_(char ** res, size_t lim);
 extern int fgetss(FILE * fin, char ** res);
-extern int fgetss_(FILE * fin, char ** res, int lim);
+extern int fgetss_(FILE * fin, char ** res, size_t lim);
 
 #endif
