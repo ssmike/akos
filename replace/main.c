5342efefa23aa71802a79a925e7639fa6c5732bf
mike <surinmike@gmail.com>
2014-03-14 18:06:25 +0400

    "replace" task

---
+++ replace/main.c
@@ -0,0 +1,39 @@
+#include "../task-1/getss.h"
+#include "stdio.h"
+#include "unistd.h"
+#include "string.h"
+#include <stdbool.h>
+
+int main() {
+    char * or;
+    char * pt;
+    char * tar;
+    int orl, ptl;
+    int i, j;
+    if (!getss(&or) || !getss(&pt) || !getss(&tar)) {
+        fprintf(stderr, "Weird error");
+        return 1;
+    }
+    orl = strlen(or);
+    ptl = strlen(pt);
+    for (i = 0; i < orl;) {
+        bool fl = true;
+        if (i + ptl > orl)
+            fl = false;
+        else {
+            for (j = 0; j < ptl; j++) {
+                if (pt[j] != or[i + j]) {
+                    fl = false;
+                    break;
+                }
+            }
+        }
+        if (fl) {
+            fprintf(stdout, "%s", tar);
+            i += ptl;
+        } else {
+            fprintf(stdout, "%c", or[i]);
+            i++;
+        }
+    }
+}
