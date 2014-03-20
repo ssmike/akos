c5fc828b7917ec4c8cc1612873915faeca660536
Alexey Salnikov <alexey.salnikov@gmail.com>
2014-03-20 18:47:27 +0400

    Применил astyle

--- task-1/getss.c
+++ task-1/getss.c
@@ -2,19 +2,23 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int getss(char ** res) {
+int getss(char ** res)
+{
     return fgetss(stdin, res);
 }
 
-int getss_(char ** res, size_t lim) {
+int getss_(char ** res, size_t lim)
+{
     return fgetss_(stdin, res, lim);
 }
 
-int fgetss(FILE * fin, char ** res) {
+int fgetss(FILE * fin, char ** res)
+{
     return fgetss_(fin, res, GETSS_DEFAULT_LIM);
 }
 
-int fgetss_(FILE * fin, char ** res, size_t lim) {
+int fgetss_(FILE * fin, char ** res, size_t lim)
+{
     int cchar;
     long psize;
     size_t sz = 0;
@@ -22,40 +26,52 @@ int fgetss_(FILE * fin, char ** res, size_t lim) {
     char * buf, * tmp;
     *res = NULL;
     psize = sysconf(_SC_PAGESIZE);
-    if (feof(stdin)) {
+    if (feof(stdin))
+    {
         return EOF_ERROR;
     }
     buf = malloc(sizeof(char));
     if (buf == NULL) return ALLOCATION_FAILED;
-    while (sz < lim && (cchar = fgetc(fin)) != '\n' && !feof(fin)) {
-        while (sz + sizeof(char) >= allsz) {
+    while (sz < lim && (cchar = fgetc(fin)) != '\n' && !feof(fin))
+    {
+        while (sz + sizeof(char) >= allsz)
+        {
             size_t newbsz = allsz * 2;
-            if (allsz > PAGES_LIMIT * psize) {
-                if (allsz % psize != 0) {
+            if (allsz > PAGES_LIMIT * psize)
+            {
+                if (allsz % psize != 0)
+                {
                     newbsz = allsz + psize - (allsz % psize);
-                } else {
+                }
+                else
+                {
                     newbsz = allsz + psize;
                 }
             }
             tmp = realloc(buf, newbsz);
-            if (tmp == NULL) {
+            if (tmp == NULL)
+            {
                 *res = buf;
                 return ALLOCATION_FAILED;
-            } else buf = tmp;
+            }
+            else buf = tmp;
             allsz = newbsz;
-        } 
+        }
         buf[sz/sizeof(char)] = cchar;
         sz += sizeof(char);
     }
-    if (sz >= lim) {
+    if (sz >= lim)
+    {
         *res = buf;
         return LIMIT_EXCEEDED;
     }
     tmp = realloc(buf, sz + sizeof(char));
-    if (tmp == NULL) {
+    if (tmp == NULL)
+    {
         *res = buf;
         return ALLOCATION_FAILED;
-    } else buf = tmp;
+    }
+    else buf = tmp;
     buf[sz/sizeof(char)] = '\0';
     *res = buf;
     return sz;
