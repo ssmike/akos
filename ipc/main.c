a0cc6ca075f08323662436e922e8eceb26086711
mike <surinmike@gmail.com>
2014-03-28 18:34:16 +0400

    ipc bugs fixed

--- ipc/main.c
+++ ipc/main.c
@@ -14,7 +14,7 @@ int main(int argc, char ** argv) {
     int fd0[2], fd1[2];
     int fd;
     int st1, st2, st3;
-    if (argc != 9) return 3;
+    /*if (argc != 10) return 3;*/
     if (pipe(fd0) == -1 || pipe(fd1) == -1) {
         printf("error");
         return 3;
@@ -25,16 +25,16 @@ int main(int argc, char ** argv) {
         close(fd0[1]);
         close(fd1[0]);
         close(fd1[1]);
-        execlp(argv[1], argv[2], argv[3]);
-    } else close(fd0[0]);
+        execlp(argv[1], argv[1], argv[2], argv[3]);
+    } 
     if (fork() == 0) {
         dup2(fd0[0], 0);
-        dup2(fd1[1], 1);
+        dup2(fd1[1], 1); 
         close(fd0[0]);
         close(fd0[1]);
         close(fd1[0]);
         close(fd1[1]);
-        execlp(argv[4], argv[5], argv[6]);
+        execlp(argv[4], argv[4], argv[5], argv[6]);
     }
     if (fork() == 0) {
         dup2(fd1[0], 0);
@@ -42,9 +42,15 @@ int main(int argc, char ** argv) {
         close(fd0[1]);
         close(fd1[0]);
         close(fd1[1]);
-        execlp("wc", "");
+        execlp("wc", "wc", 0);
     }
-    wait(&st1); wait(&st2); wait(&st3);
+    close(fd0[0]);
+    close(fd0[1]);
+    close(fd1[0]);
+    close(fd1[1]);
+    wait(&st1);
+    wait(&st2);
+    wait(&st3);
     if (st1 != 0 || st2 != 0 || st3 != 0) {
         printf("error");
         return 0;
@@ -52,5 +58,5 @@ int main(int argc, char ** argv) {
     fd = open(argv[9], O_CREAT | O_APPEND | O_WRONLY);
     dup2(fd, 1);
     close(fd);
-    execlp(argv[7], argv[8]);
+    execlp(argv[7], argv[8]); 
 }
