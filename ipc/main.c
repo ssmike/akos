0816967fcb6f557d92b013d1dc1c9bc7e1e7503f
mike <surinmike@gmail.com>
2014-03-28 19:14:44 +0400

    execlp

--- ipc/main.c
+++ ipc/main.c
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <sys/wait.h>
 #include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
 /*
  * if arg1 arg2 arg3 | arg4 arg5 arg6 | wc ; then
  *     arg7 arg8 >> arg9
@@ -10,53 +13,50 @@
  * fi
  */
 
+
+int fd0[2], fd1[2];
+int fd;
+int st1, st2, st3;
+
+void closeall() {
+    close(fd0[0]);
+    close(fd0[1]);
+    close(fd1[0]);
+    close(fd1[1]);
+}
+
 int main(int argc, char ** argv) {
-    int fd0[2], fd1[2];
-    int fd;
-    int st1, st2, st3;
-    /*if (argc != 10) return 3;*/
+    if (argc != 10) return 3;
     if (pipe(fd0) == -1 || pipe(fd1) == -1) {
         printf("error");
         return 3;
     }
     if (fork() == 0) {
         dup2(fd0[1], 1);
-        close(fd0[0]);
-        close(fd0[1]);
-        close(fd1[0]);
-        close(fd1[1]);
-        execlp(argv[1], argv[1], argv[2], argv[3]);
+        closeall();
+        execlp(argv[1], argv[1], argv[2], argv[3], NULL);
     } 
     if (fork() == 0) {
         dup2(fd0[0], 0);
         dup2(fd1[1], 1); 
-        close(fd0[0]);
-        close(fd0[1]);
-        close(fd1[0]);
-        close(fd1[1]);
-        execlp(argv[4], argv[4], argv[5], argv[6]);
+        closeall();
+        execlp(argv[4], argv[4], argv[5], argv[6], NULL);
     }
     if (fork() == 0) {
         dup2(fd1[0], 0);
-        close(fd0[0]);
-        close(fd0[1]);
-        close(fd1[0]);
-        close(fd1[1]);
-        execlp("wc", "wc", 0);
+        closeall();
+        execlp("wc", "wc", NULL);
     }
-    close(fd0[0]);
-    close(fd0[1]);
-    close(fd1[0]);
-    close(fd1[1]);
+    closeall();
     wait(&st1);
     wait(&st2);
     wait(&st3);
     if (st1 != 0 || st2 != 0 || st3 != 0) {
-        printf("error");
+        fprintf(stderr, "error");
         return 0;
     }
-    fd = open(argv[9], O_CREAT | O_APPEND | O_WRONLY);
+    fd = open(argv[9], O_CREAT | O_APPEND | O_WRONLY , S_IRWXU);
     dup2(fd, 1);
     close(fd);
-    execlp(argv[7], argv[8]); 
+    execlp(argv[7], argv[7], argv[8], NULL);
 }
